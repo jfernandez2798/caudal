ff7125685b36f1a685f1b086d224f35c28365c72
GenomaVoid <Genoma_void@hotmail.com>
2019-05-01 15:30:48 -0600

    se agregego el proyecto de ionic

---
+++ ionic-4-tutorial/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzByMxJGhoZLFJmTrQ52tW0mpeJi0vnqo",
+    authDomain: "ionic-auth-example-87f5a.firebaseapp.com",
+    databaseURL: "https://ionic-auth-example-87f5a.firebaseio.com",
+    projectId: "ionic-auth-example-87f5a",
+    storageBucket: "ionic-auth-example-87f5a.appspot.com",
+    messagingSenderId: "874008896122"
+
+  }
+};
